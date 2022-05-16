f93f7f689d36180a42aa40a59a5ab50d730aa747
ZHENG <18823776746@qq.com>
2022-05-16 15:03:05 +0800

    清空

--- src/components/TablePro/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: ZHENG
- * @Date: 2022-04-30 14:35:52
- * @LastEditors: ZHENG
- * @LastEditTime: 2022-04-30 14:37:17
- * @FilePath: \admin\src\components\TablePro\index.ts
- * @Description:
- */
-export { default as BasicTable } from './src/Table.vue';
--- src/components/TablePro/src/Props.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: ZHENG
- * @Date: 2022-04-30 15:54:32
- * @LastEditors: ZHENG
- * @LastEditTime: 2022-04-30 15:54:33
- * @FilePath: \admin\src\components\TablePro\src\Props.ts
- * @Description:
- */
